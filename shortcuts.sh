a127005db9e15eb73e339c69a8b11a87cd6db875
lucashelfs <lucashelfstein@gmail.com>
2018-07-15 23:25:57 -0300

    Teclado oficial do Dell

--- shortcuts.sh
+++ shortcuts.sh
@@ -4,6 +4,9 @@ xrandr --output HDMI-1 --left-of eDP-1
 # Mudar para teclado mecânico 
 setxkbmap us -variant intl
 
+# Teclado padrão
+# setxkbmap -model abnt2 -layout br -variant abnt2
+
 # Volume
 #pavucontrol
 
