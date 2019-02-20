246095959d04635b27c01e9273f996294301fbe6
lucashelfs <lucashelfstein@gmail.com>
2019-02-20 16:41:07 -0300

    Multimedia + volume keys

---
+++ instalar_coisas.sh
@@ -0,0 +1,7 @@
+sudo apt update
+
+wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/libplayerctl2_2.0.1-1_amd64.deb
+wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/playerctl_2.0.1-1_amd64.deb
+
+sudo dpkg -i libplayerctl2_2.0.1-1_amd64.deb playerctl_2.0.1-1_amd64.deb
+sudo apt install xbacklight
